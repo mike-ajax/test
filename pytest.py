0b35ff6b6b9b1ae04146923d2eb2ad2aef877b38
root <root@de2019g22.de-home>
2019-10-03 21:50:45 +0200

    addition in pytest.py

--- pytest.py
+++ pytest.py
@@ -2,6 +2,10 @@
 
 def main():
     print("this is the python main file")
+    a = 5
+    b = 9
+    c = a + b
+    print("c=a+b= " , a , " + " , b , "=", c)
 
 if __name__ == '__main__':
     main()
